235e586a4c5264ac4a55c7d51f8846ce91939640
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-01-24 07:57:59 +0100

    :tada: Add training files

---
+++ es2015/02-desctructuring-objects/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+const userInfos = {
+    firstName: "Abdessalam",
+    lastName: "Benharira",
+    city: "Paris",
+    state: "France",
+    zipCode: 75010
+};
+
+// const {firstName, lastName} = userInfos;
+const { firstName: fName, lastName: lName } = userInfos;
+
+console.log(`Hello I'm ${fName} ${lName}`);
